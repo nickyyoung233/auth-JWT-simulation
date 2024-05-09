f7f320dab0563609f2674592caf6a7df26904b9e
16684814105 <63778248+nickyyoung233@users.noreply.github.com>
2024-05-10 07:01:11 +0800

    用户登录校验相关的模拟逻辑

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# JWT-simulate
- 请求和刷新token模拟
+## Authentication-simulate
+
+用户登录校验的模拟
+
+### 相关文件
+
+- auth-server.js
+- authentication.rest
+
+### 模拟方式
+
+1. `npm run auth` 运行 auth-server.js
+2. 采用 authentication.rest 进行数据请求
+
+## JWT-simulate
+
+请求和刷新 token 模拟
---
+++ auth-server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/users", (req, res) => {
+  console.log("GET /users");
+  // res.status(200).json(users);
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  // 理解 bcrypt
+  try {
+    //方案1
+    // const salt = await bcrypt.genSalt();
+    // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // console.log("salt:", salt);
+
+    //方案2
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log("hashedPassword:", hashedPassword);
+
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    //   res.status(201).json(user);
+    res.status(201).send("User Created");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(5050, () => {
+  console.log("server is on 5050");
+});
